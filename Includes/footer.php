5d05b70fa3c26f807740cdf2e77b8940f004499e
CharizardXU <125834570+CharizardXU@users.noreply.github.com>
2023-03-05 10:00:49 -0700

    Main footer.

---
+++ Includes/footer.php
@@ -0,0 +1,71 @@
+            <nav class="footer">
+        <main class="container">
+          <img
+            src="/assets/images/bp-primary1.png"
+            class="d-inline-block mb-3"
+            width="256"
+          />
+          <div class="d-flex justify-content-center gap-4 text-2xl mb-4">
+            <a target="_blank" href="https://www.tiktok.com/@<?php echo $siteName; ?>" class="text-light"><i class="fab fa-tiktok"></i></a>
+            <a target="_blank" href="https://discord.gg/<?php echo $siteName; ?>" class="text-light"><i class="fab fa-discord"></i></a>
+            <a target="_blank" href="https://www.twitch.tv/<?php echo $siteName; ?>" class="text-light"><i class="fab fa-twitch"></i></a>
+            <a target="_blank" href="https://www.twitter.com/<?php echo $siteName; ?>" class="text-light"><i class="fab fa-twitter"></i></a>
+            <a target="_blank" href="https://www.youtube.com/c/<?php echo $siteName; ?>" class="text-light"><i class="fab fa-youtube"></i></a>
+            <a target="_blank" href="https://www.instagram.com/<?php echo $siteName; ?>/" class="text-light"><i class="fab fa-instagram"></i></a>
+          </div>
+          <main class="divider divider-centered mb-4">Other Links</main>
+          <div class="row gy-4 mb-5">
+            <div class="col-md-4 text-center">
+              <a href="/home" class="footer-link">Home</a>
+            </div>
+            <div class="col-md-4 text-center">
+              <a href="/download" class="footer-link">Download</a>
+            </div>
+            <div class="col-md-4 text-center">
+              <a href="/" class="footer-link">Game Docs</a>
+            </div>
+            <div class="col-md-4 text-center">
+              <a href="#" class="footer-link">Help Desk</a>
+            </div>
+            <div class="col-md-4 text-center">
+              <a href="https://blog.<?php echo $siteLink; ?>/" class="footer-link">Blog</a>
+            </div>
+            <div class="col-md-4 text-center">
+              <a href="/bugs/send" class="footer-link">Report A Bug</a>
+            </div>
+          </div>
+          <div class="mb-2">
+            <a href="/about/terms-and-conditions" class="text-light text-decoration-none fw-semibold"
+              >Terms and Conditions</a
+            >
+            <span class="mx-2 text-muted">&bullet;</span>
+            <a href="/about/privacy-policy" class="text-light text-decoration-none fw-semibold"
+              >Privacy Policy</a
+            >
+            <span class="mx-2 text-muted">&bullet;</span>
+            <a href="#" class="text-light text-decoration-none fw-semibold"
+              >Jobs</a
+            >
+            <span class="mx-2 text-muted">&bullet;</span>
+            <a href="/about/contact-us" class="text-light text-decoration-none fw-semibold"
+              >Contact Us</a
+            >
+          </div>
+          <div class="text-muted text-sm">
+            &copy; 2023 <span class="fw-semibold"><?php echo $siteName; ?>, Inc.</span> All
+            rights reserved.
+          </div>
+        </main>
+      </nav>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+    <script src="/assets/js/jquery.timeago.js"></script>
+    <script src="/assets/js/main.js?r=1"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js" integrity="sha512-lteuRD+aUENrZPTXWFRPTBcDDxIGWe5uu0apPEn+3ZKYDwDaEErIK9rvR0QzUGmUQ55KFE2RqGTVoZsKctGMVw==" crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
